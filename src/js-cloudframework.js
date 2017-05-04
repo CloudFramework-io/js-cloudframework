235684f7c03a5993018bcd92d0eb4b238287aa37
Adrian Martinez <am@bloombees.com>
2017-05-04 15:48:05 +0200

    Update to v1.1.8

--- src/js-cloudframework.js
+++ src/js-cloudframework.js
@@ -1,6 +1,6 @@
 Core = new function () {
 
-    this.version = '1.1.6';
+    this.version = '1.1.8';
     this.debug = false;
     this.authActive = false;
     this.authCookieName = 'cfauth';
@@ -866,7 +866,7 @@ Core = new function () {
                 if(typeof data.template.object == 'string')  object = data.template.object;
                 if(typeof data.template.id == 'string')  id = data.template.id;
                 if(typeof data.template.type == 'string')  type = data.template.type;
-                if(typeof data.template.dom == object)  dom = data.template.dom;
+                if(typeof data.template.dom == 'object')  dom = data.template.dom;
             }
             if(url=='') {
                 Core.error.add('Core.dynamic.loadTemplate(data,callback) Missing a right value for data. use {template:url}');
